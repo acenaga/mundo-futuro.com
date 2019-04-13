346be41e25f41eacfff1c1cd02b2ffbafcb27f8c
acenaga <mundofuturoca@gmail.com>
2019-04-12 19:47:19 -0500

    Starting the layout of the Mundo Futuro page

---
+++ index.php
@@ -0,0 +1,76 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS ver 4.3.1-->
+    <link rel="stylesheet" href="assets/plugins/bootstrap/css/bootstrap.min.css">
+    <!-- Fotn Awesome Ver 5.8.1-->
+    <link rel="stylesheet" href="assets/plugins/fontawesome/css/all.min.css">
+    <!-- General Styles-->
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <title>Mundo Futuro</title>
+  </head>
+  <body>
+    <!-- Header & Nav -->
+    <header>
+        <img class="logo-nav" src="assets/images/logo-nav.png" alt="">
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link active" href="#">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Quienes Somos</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="#">Servicios</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link disabled" href="#">Contactos</a>
+        </li>
+    </ul>
+    </header>
+    <!-- page begin -->
+    <div class="container-fluid">
+        <!-- Slider -->
+        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+            <ol class="carousel-indicators">
+                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+            </ol>
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img class="d-block w-100" src="assets/images/slider-1.jpg" alt="First slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="assets/images/slider-1.jpg" alt="Second slide">
+                </div>
+                <div class="carousel-item">
+                    <img class="d-block w-100" src="assets/images/slider-1.jpg" alt="Third slide">
+                </div>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+
+
+    </div>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="assets/plugins/jquery/jquery-3.3.1.slim.min.js"></script>
+    <!-- popper Ver 1.14.7 -->
+    <script src="assets/plugins/popper/popper.min.js"></script>
+    <script src="assets/plugins/bootstrap/js/bootstrap.min.js"></script>
+  </body>
+</html>
