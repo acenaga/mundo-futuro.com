5043cdbe7cbc703b99eb9f0dc4090e18ec08d5c8
acenaga <mundofuturoca@gmail.com>
2019-04-15 16:40:32 -0500

    Se agrego la seccion de Quienes somos

--- index.php
+++ index.php
@@ -17,50 +17,71 @@
   <body>
     <!-- Header & Nav -->
     <header>
-        <img class="logo-nav" src="assets/images/logo-nav.png" alt="">
-    <ul class="nav">
-        <li class="nav-item">
-            <a class="nav-link active" href="#">Home</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Quienes Somos</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link" href="#">Servicios</a>
-        </li>
-        <li class="nav-item">
-            <a class="nav-link disabled" href="#">Contactos</a>
-        </li>
-    </ul>
+        <div class="container">
+            <div class="row">
+                <div class="logo-nav">
+                    <img src="assets/images/logo-nav.png" class="img-fluid" alt="Responsive image">
+                </div>
+                <ul class="nav">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Quienes Somos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Servicios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link disabled" href="#">Contactos</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </header>
     <!-- page begin -->
     <div class="container-fluid">
         <!-- Slider -->
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-            <ol class="carousel-indicators">
-                <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-                <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img class="d-block w-100" src="assets/images/slider-1.jpg" alt="First slide">
+        <div class="row">
+            <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                <ol class="carousel-indicators">
+                    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img class="d-block w-100" src="assets/images/slider-1.jpg" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="assets/images/slider-1.jpg" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                        <img class="d-block w-100" src="assets/images/slider-1.jpg" alt="Third slide">
+                    </div>
                 </div>
-                <div class="carousel-item">
-                    <img class="d-block w-100" src="assets/images/slider-1.jpg" alt="Second slide">
-                </div>
-                <div class="carousel-item">
-                    <img class="d-block w-100" src="assets/images/slider-1.jpg" alt="Third slide">
+                <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                </a>
+                <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <div>
+                    <h5>¿QUIENES SOMOS?</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi accusamus expedita quidem aliquam atque dicta voluptate, placeat corporis ex quae ipsa optio exercitationem sed et magni esse dolorum vel voluptas.</p>
+                    <button type="button" class="btn btn-primary">CONTACTO</button>
                 </div>
             </div>
-            <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="sr-only">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="sr-only">Next</span>
-            </a>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <img src="assets/images/quienes.jpg" class="img-fluid" alt="Responsive image">
+            </div>
+
         </div>
 
 
