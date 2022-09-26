b39a32a82d034411623dab8f8d647113f279d4d1
Carlos Ferrer <Carlos.Alberto@Sci-us.com>
2022-09-26 17:25:22 -0300

    changing the index page

---
+++ resources/views/index.blade.php
@@ -0,0 +1,14 @@
+@extends('layouts.app')
+
+@section('content')
+    <section class="comming-soon container-fluid">
+        <div class="row">
+            <div class="col-12 d-flex justify-content-center align-items-center">
+                <img src="{{ asset('../assets/img/horizontal-logo.svg') }}" alt="aqui va una imagen">
+                <div class="rocket">
+
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
