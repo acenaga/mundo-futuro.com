7ae30fd6f991fcdae7ac3aa86a75e147f6a39862
Carlos Ferrer <Carlos.Alberto@Sci-us.com>
2022-09-26 20:02:53 -0300

    modifying web.php

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,8 @@ Route::get('/', function () {
     return view('cooming');
 });
 
+
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
