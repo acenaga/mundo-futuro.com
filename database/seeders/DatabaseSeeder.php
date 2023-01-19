38812c1467daba4a131b6423a58a6b05a449c4a2
Carlos Ferrer <mundofuturoca@gmail.com>
2023-01-19 09:49:26 -0300

    adding password and user in seeders & updaed to laravel 9.47

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -26,7 +26,7 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'username' => 'acenaga',
-            'email' => 'mundofuturoca@gmail.com',
+            'email' => 'mundofuturoca@gmail.com','password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         ]);
 
         Category::factory(5)->create();
