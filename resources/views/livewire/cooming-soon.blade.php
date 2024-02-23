46fe53222b2352768956ec5591f0c3127f15bc79
Carlos Ferrer <mundofuturoca@gmail.com>
2024-02-23 09:48:37 -0300

    delete a component livewire cooming soon

--- resources/views/livewire/cooming-soon.blade.php
+++
@@ -1,9 +0,0 @@
-<div>
-    <section class="comming-soon container">
-        <div class="row">
-            <div class="col-12 d-flex justify-content-center align-items-center">
-                <img src="{{ asset('../assets/img/horizontal-logo.svg') }}" alt="aqui va una imagen">
-            </div>
-        </div>
-    </section>
-</div>
