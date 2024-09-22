22d36e037e1f14a5231812542d2ada569f977379
Carlos Ferrer <mundofuturoca@gmail.com>
2024-09-22 19:16:31 -0300

    add meta robots no index

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -27,7 +27,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('assets/css/custom.css') }}">
 </head>
-@if (env('APP_ENV') != 'local')
+@if (env('APP_ENV') === 'production')
     <!-- Google tag (gtag.js) -->
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-1HTHPRCFMN"></script>
     <script>
